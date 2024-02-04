a5f530b310261fc9104aadb98357f6ac0a87ed96
JeroenvdV <JeroenvdV@users.noreply.github.com>
2024-02-04 16:22:43 +0100

    Connect env vars

--- scripts/remove-key-from-git.sh
+++ scripts/remove-key-from-git.sh
@@ -17,4 +17,4 @@ export RKFG_FILES=$(git reflog --pretty=format:"%H" | sort | uniq | xargs -I % s
 # Goes into all commits and executes the replace command on all possible filenames.
 # Uses `grep -s -I -q .` to silently check if a file is binary and exists in the working tree.
 # Uses perl to replace all the instances of $APIKEY is the found non-binary files.
-echo "Replacing key: $APIKEY" && git filter-branch --tree-filter 'for item in ${RKFG_FILES} ; do grep -s -I -q . $item && echo $item && perl -pi -e "s/$APIKEY/apikey/g" $item || true; done'
+echo "Replacing key: $APIKEY" && git filter-branch --tree-filter 'for item in ${RKFG_FILES} ; do grep -s -I -q . $item && echo $item && perl -pi -e "s/$APIKEY/xxxx/g" $item || true; done'
