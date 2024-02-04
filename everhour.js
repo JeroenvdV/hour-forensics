d9334f331a6af69ef2e7bac5608129001becd2c8
JeroenvdV <JeroenvdV@users.noreply.github.com>
2024-02-04 16:22:43 +0100

    Working addTime

--- everhour.js
+++ everhour.js
@@ -1,12 +1,10 @@
-import request from "request";
-
 const USER = xxxx;
 const HEADERS = {
   "Content-Type": "application/json",
   "X-Api-Key": "apikey",
 };
 
-function addTime(task, seconds, date, comment = "") {
+async function addTime(task, seconds, date, comment = "") {
   const body = JSON.stringify({
     "time": seconds,
     "user": USER,
@@ -16,16 +14,20 @@ function addTime(task, seconds, date, comment = "") {
 
   const url = `https://api.everhour.com/tasks/${task}/time`;
 
-  return request({
-    method: "POST",
-    headers: HEADERS,
-    url,
-    body,
-  }, function(error, response, body) {
-    console.log("Status:", response.statusCode);
-    console.log("Headers:", JSON.stringify(response.headers));
-    console.log("Response:", body);
-  });
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: HEADERS,
+      body
+    });
+    const responseBody = await response.json();
+
+    console.log("Status:", response.status);
+    console.log("Headers:", JSON.stringify(Object.fromEntries(response.headers.entries())));
+    console.log("Response:", responseBody);
+  } catch (error) {
+    console.error("Fetch Error:", error);
+  }
 }
 
-addTime("xxxx-19235", 111, "2024-02-03");
+await addTime("xxxx-19235", 111, "2024-02-03");
